6d1c02491332e176192906252a559c02ee7adb26
ArtemShtanko <artem.shtanko@gmail.com>
2023-03-29 17:04:02 +0300

    Update lesson_01.py

--- lesson_01.py
+++ lesson_01.py
@@ -1,11 +1,35 @@
 
+
+# # math = a,b,c, = int(input()), int(input()), int(input())
+
+# math = a,b,c, = "One", "Two", "Three"
+
+
+# math_change = math[1], "New Text"
+
+# # math_change = a, "New Text", b
+
+# #math[1] = "New Text"
+
+
+# # math_change = "TEST_CHANGE"
+
+# print (math_change)
+
+
+
+
+math = a,b,c, = "Hello", "World", "!"
+b = "Python"
+
+print (math[0], b, math[-1])
+
+
+
+
 message = "Hello World!"
 message_changed_invert = message [::-1] # message_invert ## !dlroW olleH
 message_changed_delete = message [::2] # delete each 2 symbol from the begin ## HloWrd
 message_changed_delete_invert = message [::-2] # delete each 2 symbol from the end ## !lo le
 
 print (message, "->", message_changed_invert, "->", message_changed_delete, "->", message_changed_delete_invert) # Hello World! -> !dlroW olleH -> !lo le
-
-
-
-
