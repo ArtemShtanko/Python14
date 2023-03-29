11644f1e9f90fc39e357986c1a2de23f22de50fe
ArtemShtanko <artem.shtanko@gmail.com>
2023-03-29 15:20:10 +0300

    Update lesson_01.py

--- lesson_01.py
+++ lesson_01.py
@@ -1,5 +1,11 @@
 
 message = "Hello World!"
-message_changed = message [::-1] # message_invert
+message_changed_invert = message [::-1] # message_invert ## !dlroW olleH
+message_changed_delete = message [::2] # delete each 2 symbol from the begin ## HloWrd
+message_changed_delete_invert = message [::-2] # delete each 2 symbol from the end ## !lo le
 
-print (message, "->", message_changed)
+print (message, "->", message_changed_invert, "->", message_changed_delete, "->", message_changed_delete_invert) # Hello World! -> !dlroW olleH -> !lo le
+
+
+
+
