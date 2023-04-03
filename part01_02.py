74b457a7b5de2053b7f7bd07a45c65c3815f29f1
ArtemShtanko <artem.shtanko@gmail.com>
2023-04-03 11:05:15 +0300

    Update part01_02.py

--- part01_02.py
+++ part01_02.py
@@ -1,7 +1,12 @@
 
-a = "hello python"
+# a = "hello python"
 
-u = a.upper()
-t = a.title()
+# u = a.upper()
+# t = a.title()
 
-print(u, t, sep="!!! ", end="!!!")
+# print(u, t, sep="!!! ", end="!!!")
+
+
+c = int(4 ** 0.5)
+print (c)
+
