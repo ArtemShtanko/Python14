ec9651d54427abc147afedcfe5381335cee9fc4c
ArtemShtanko <artem.shtanko@gmail.com>
2023-04-03 11:05:12 +0300

    Update my_test.py

--- my_test.py
+++ my_test.py
@@ -1,18 +1,23 @@
 
 
-while True:
-    number = int(input("Please input number:"))
-    user_choise = input("Y or N")
-    if user_choise == "Y":
-        while True:
-         print(number)
-         number = number - 1
-         if number <= 0:
-            break
-        break
-    if user_choise == "N":
-        break
-print("User choose:", user_choise)
+# while True:
+#     number = int(input("Please input number:"))
+#     user_choise = input("Y or N")
+#     if user_choise == "Y":
+#         while True:
+#          print(number)
+#          number = number - 1
+#          if number <= 0:
+#             break
+#         break
+#     if user_choise == "N":
+#         break
+# print("User choose:", user_choise)
 
 
+a = int(input("Input a:"))
+b = int(input("Input b:"))
 
+test = a + b
+
+print(test)
