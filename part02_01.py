89afdf9813bd7f4e2424762c804ba459f67c3476
ArtemShtanko <artem.shtanko@gmail.com>
2023-04-03 23:22:23 +0300

    Update part02_01.py

--- part02_01.py
+++ part02_01.py
@@ -41,27 +41,28 @@
 
 ##########################################################################
 
-name = input("Name:")
-age = input("Age:")
-has_driver_licence = input("Choose Yes or No:")
+# name = input("Name:")
+# age = input("Age:")
+# has_driver_licence = input("Choose Yes or No:")
 
-try:
-    age = int(age)
-    # if age == int(age):
-    #     True
+# try:
+#     age = int(age)
+#     # if age == int(age):
+#     #     True
 
-    if name and age >= 18 and has_driver_licence == "Yes":
-        print(f"User {name} can rent a car")
+#     if name and age >= 18 and has_driver_licence == "Yes":
+#         print(f"User {name} can rent a car")
 
-### the same result ###
-    # if name and age >= 18 and "Yes" in has_driver_licence:
-    #     print(f"User {name} can rent a car")
+# ### the same result ###
+#     # if name and age >= 18 and "Yes" in has_driver_licence:
+#     #     print(f"User {name} can rent a car")
 
-    else:
-        print("You cannot rent a car")
+#     else:
+#         print("You cannot rent a car")
 
-except ValueError:
-    print(f" \"{age}\" is not a number")
+# except ValueError:
+#     print(f" \"{age}\" is not a number")
 
 ##########################################################################
+
 
