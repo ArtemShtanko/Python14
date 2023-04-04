eee24f680fb1905b6a6c7de3d0ccd3c7219aade8
ArtemShtanko <artem.shtanko@gmail.com>
2023-04-04 17:58:09 +0300

    Create mac_address_v3.py

---
+++ mac_address_v3.py
@@ -0,0 +1,55 @@
+
+mac_original = input("Input mac address:")
+
+# if "a" in mac_original or "b" in mac_original or "c" in mac_original or "d" in mac_original or "e" in mac_original or "f" in mac_original:
+    # print(bool(mac_original))
+#     print(type(mac_original))
+
+# if mac_original.find("a", "b", "c"):
+
+# if mac_original == mac_original.find("a", "b", "c"):
+#     if mac_original == mac_original.find("d" or "e" or "f"):
+#         print ("ok")
+
+# else:
+#     print("not ok")
+
+
+if mac_original.find("d"):
+    if mac_original.find("5"):
+        print("Test finished")
+
+
+
+
+# if ":" or "-" in mac_original:
+#     mac_original = mac_original.replace(":" or "-", "")
+
+
+#     if mac_original == mac_original[:12]:
+#         add = "-"
+#         mac_original_2 = mac_original[0:4] + add + mac_original[4:8] + add + mac_original[8:]
+#         mac_original_2_lower = mac_original_2.lower()
+#         print(mac_original_2_lower)
+# else:
+#     print ("Enter correct value")
+
+
+
+
+
+
+
+
+
+
+#     if ":" or "-" in mac_original:
+#         mac_original = mac_original.replace(":" or "-", "")
+
+#         if mac_original == mac_original[:12]:
+#             add = "-"
+#             mac_original_2 = mac_original[0:4] + add + mac_original[4:8] + add + mac_original[8:]
+#             mac_original_2_lower = mac_original_2.lower()
+#             print(mac_original_2_lower)
+# else:
+#     print ("Enter correct value")
