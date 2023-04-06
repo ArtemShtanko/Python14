261dc1966892c2f3193f87cb4d5e6382738889eb
ArtemShtanko <artem.shtanko@gmail.com>
2023-04-06 10:02:27 +0300

    Update mac_address_v3.py

--- mac_address_v3.py
+++ mac_address_v3.py
@@ -1,55 +1,99 @@
 
-mac_original = input("Input mac address:")
-
-# if "a" in mac_original or "b" in mac_original or "c" in mac_original or "d" in mac_original or "e" in mac_original or "f" in mac_original:
-    # print(bool(mac_original))
-#     print(type(mac_original))
-
-# if mac_original.find("a", "b", "c"):
-
-# if mac_original == mac_original.find("a", "b", "c"):
-#     if mac_original == mac_original.find("d" or "e" or "f"):
-#         print ("ok")
-
-# else:
-#     print("not ok")
-
-
-if mac_original.find("d"):
-    if mac_original.find("5"):
-        print("Test finished")
-
-
-
+# mac_original = input("Input mac address:")
 
 # if ":" or "-" in mac_original:
 #     mac_original = mac_original.replace(":" or "-", "")
 
-
 #     if mac_original == mac_original[:12]:
 #         add = "-"
-#         mac_original_2 = mac_original[0:4] + add + mac_original[4:8] + add + mac_original[8:]
-#         mac_original_2_lower = mac_original_2.lower()
-#         print(mac_original_2_lower)
-# else:
-#     print ("Enter correct value")
+#         mac_new = mac_original[0:4] + add + mac_original[4:8] + add + mac_original[8:]
+#         mac_new_lower = mac_new.lower()
+#         print(mac_new_lower)
+#     else:
+#         print ("Length of mac address is incorrect")
+
+#########################################################################################
 
 
+# mac_original = input("Input mac address:")
 
+# if ":" or "-" in mac_original:
+#     mac_original = mac_original.replace(":" or "-", "")
 
-
-
-
-
-
-
-#     if ":" or "-" in mac_original:
-#         mac_original = mac_original.replace(":" or "-", "")
-
+#     if True:
+#         try:
+#             num = int(mac_original, 16)
+#         except ValueError:
+#             print("Mac address is incorrect")     
+    
 #         if mac_original == mac_original[:12]:
 #             add = "-"
-#             mac_original_2 = mac_original[0:4] + add + mac_original[4:8] + add + mac_original[8:]
-#             mac_original_2_lower = mac_original_2.lower()
-#             print(mac_original_2_lower)
+#             mac_new = mac_original[0:4] + add + mac_original[4:8] + add + mac_original[8:]
+#             mac_new_lower = mac_new.lower()
+#             print(mac_new_lower)
+#         else:
+#             print ("Length of mac address is incorrect")
+
+#########################################################################################
+
+# mac_original = input("Input mac address:")
+
+# mac_original = mac_original.replace(":" or "-", "")
+# mac_original = mac_original.lower()
+
+# if True:
+#     try:
+#         num = int(mac_original, 16)
+#     except ValueError:
+#         print("Mac address is incorrect")     
+
+# if mac_original == mac_original[:12]:
+#     add = "-"
+#     mac_new = mac_original[0:4] + add + mac_original[4:8] + add + mac_original[8:]
+#     mac_new_lower = mac_new.lower()
+#     print(mac_new_lower)
 # else:
-#     print ("Enter correct value")
+#     print ("Length of mac address is incorrect")
+
+#########################################################################################
+
+# mac_original = input("Input mac address:")
+# add = "-"
+# mac_original = mac_original.replace(":" or "-", "")
+# mac_original = mac_original.lower()
+
+# if True:
+#     try:
+#         num = int(mac_original, 16)
+#     except ValueError:
+#         print("Mac address is incorrect")
+        
+# if mac_original == mac_original[:12]:
+#     mac_new = mac_original[0:4] + add + mac_original[4:8] + add + mac_original[8:]
+#     mac_new_lower = mac_new.lower()
+#     print(mac_new_lower)
+
+# else:
+#     print ("Length of mac address is incorrect")
+
+#########################################################################################
+
+mac_original = input("Input mac address:")
+add = "-"
+mac_original = mac_original.replace(":" or "-", "")
+mac_original = mac_original.lower()
+
+try:
+
+    num = int(mac_original, 16)
+
+    if mac_original == mac_original[:12] and num:
+        mac_new = mac_original[0:4] + add + mac_original[4:8] + add + mac_original[8:]
+        mac_new_lower = mac_new.lower()
+        print(mac_new_lower)
+
+    else:
+        print ("Length of mac address is incorrect")
+
+except ValueError:
+    print("Mac address is incorrect")
